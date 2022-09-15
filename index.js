de263bb5ff54b976e5cbfa2e8c33ae66dd9f0340
D Samal <samal@smarterdharma.com>
2022-09-15 11:37:46 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express =  require('express')
+const morgan =  require('morgan')
+const app = express()
+const port =  3000
+app.use(morgan('dev'))
+
+app.get('/user', (req, res) => {
+    let result = {id:1,name:'user1'}
+    console.log(`route- USER_DETAILS, DB - ${process.env.DB_NAME}`)
+    res.status(200).json(result)
+})
+
+app.listen(port, () => {
+console.log(`app is listening on port at http://localhost:${port}`)
+})
